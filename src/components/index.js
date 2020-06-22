63c5638d60aebb0d836499aa48fc39eb45eac848
Brianna Keune <briannakeune@gmail.com>
2020-06-22 18:47:57 -0500

    file setup

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import GridSizeForm from "./GridSizeForm.js";
+import Grid from "./Grid.js";
+import Controllers from "./Controllers.js";
+
+export { GridSizeForm, Grid, Controllers };
---
+++ src/helpers/index.js
@@ -0,0 +1,5 @@
+import create_grid from "./create_grid.js";
+import clear_grid from "./clear_grid.js";
+import change_grid_size from "./change_grid_size.js";
+
+export { create_grid, clear_grid, change_grid_size };
