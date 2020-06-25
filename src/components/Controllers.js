94e664d161078b274975000d0a5d09e5c4060892
Brianna Keune <briannakeune@gmail.com>
2020-06-25 15:26:19 -0500

    updated controller buttons to use reducer

--- src/components/Controllers.js
+++ src/components/Controllers.js
@@ -1,10 +1,34 @@
 import React from "react";
 
-export default function Controllers({ clear, rows, cols, setDisplayGrid }) {
+export default function Controllers({ state, dispatch }) {
+  function start() {
+    dispatch({ type: "start" });
+  }
+  function stop() {
+    dispatch({ type: "stop" });
+  }
+  function randomize() {
+    stop();
+    dispatch({ type: "create_random" });
+  }
+  function clear() {
+    if (state.running) {
+      alert("please stop the simulation\nbefore trying to clear the board");
+      return;
+    }
+    dispatch({ type: "clear" });
+  }
+  function step() {
+    dispatch({ type: "step" });
+  }
   return (
     <div className="controller-container">
-      {/* clear button */}
-      <button onClick={() => clear(setDisplayGrid, rows, cols)}>Clear</button>
+      <button onClick={randomize}>Random Seed</button>
+      <button onClick={step}>Next Gen</button>
+      <button onClick={state.running ? stop : start}>
+        {state.running ? "Stop" : "Start"}
+      </button>
+      <button onClick={clear}>Clear</button>
     </div>
   );
 }
